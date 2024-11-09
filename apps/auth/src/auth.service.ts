f1a6e9d1e7a6a700ec859d60668882369ccf9b64
jpp000 <141282511+jpp000@users.noreply.github.com>
2024-11-09 13:02:31 -0300

    auth service

--- apps/auth/src/auth.service.ts
+++ apps/auth/src/auth.service.ts
@@ -1,8 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { UserDocument } from './users/models/user.schema';
+import { Response } from 'express';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { TokenPayload } from './interfaces/token-payload.interface';
 
 @Injectable()
 export class AuthService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async login(user: UserDocument, res: Response) {
+    const payload: TokenPayload = {
+      userId: user._id.toHexString(),
+    };
+
+    const expires = new Date();
+    expires.setSeconds(
+      expires.getSeconds() + this.configService.get('JWT_EXPIRATION'),
+    );
+
+    const token = this.jwtService.sign(payload);
+
+    res.cookie('Authentication', token, {
+      httpOnly: true,
+      expires,
+    });
+
+    return token;
   }
 }
