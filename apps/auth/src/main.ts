20836b635c4d1becb3fb677f817794d7e88fe736
jpp000 <141282511+jpp000@users.noreply.github.com>
2024-11-10 09:47:32 -0300

    add tcp transport config on auth

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -4,9 +4,11 @@ import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
 import { ConfigService } from '@nestjs/config';
 import * as cookieParser from 'cookie-parser';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
+  app.connectMicroservice({ transport: Transport.TCP });
   app.use(cookieParser());
   app.useGlobalPipes(
     new ValidationPipe({
@@ -15,6 +17,7 @@ async function bootstrap() {
   );
   app.useLogger(app.get(Logger));
   const configService = app.get(ConfigService);
+  await app.startAllMicroservices();
   await app.listen(configService.get('PORT'));
 }
 bootstrap();
