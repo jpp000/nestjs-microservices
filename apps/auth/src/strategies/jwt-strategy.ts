b54b8cd2c5e705332116adea22c28e190042b75b
jpp000 <141282511+jpp000@users.noreply.github.com>
2024-11-10 10:21:50 -0300

    stract token for diferent requests

--- apps/auth/src/strategies/jwt-strategy.ts
+++ apps/auth/src/strategies/jwt-strategy.ts
@@ -4,7 +4,6 @@ import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { TokenPayload } from '../interfaces/token-payload.interface';
 import { UsersService } from '../users/users.service';
-import { Request } from 'express';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -14,7 +13,9 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromExtractors([
-        (request: Request) => request?.cookies?.Authentication,
+        (request: any) =>
+          request?.cookies?.Authenticationrequest?.cookies?.Authentication ||
+          request?.Authentication,
       ]),
       secretOrKey: configService.get('JWT_SECRET'),
     });
