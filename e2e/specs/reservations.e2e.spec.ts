64373c3dcc1f13282f1f88b61af55ac32a2f90de
jpp000 <141282511+jpp000@users.noreply.github.com>
2024-11-13 19:33:57 -0300

    e2e tests create & get reservations

--- e2e/specs/reservations.e2e.spec.ts
+++ e2e/specs/reservations.e2e.spec.ts
@@ -1,4 +1,6 @@
 describe('Reservations', () => {
+  let jwt: string;
+
   beforeAll(async () => {
     const user = {
       email: 'test@gmail.com',
@@ -12,18 +14,57 @@ describe('Reservations', () => {
       },
     });
 
-    const auth_res = await fetch('http://auth:3001/auth/login', {
+    const res = await fetch('http://auth:3001/auth/login', {
       method: 'POST',
       body: JSON.stringify(user),
       headers: {
         'Content-Type': 'application/json',
       },
     });
-    const jwt = await auth_res.text();
-    console.log(jwt);
+    const json = await res.json();
+    jwt = json.jwt;
   });
 
-  test('Create', () => {
-    expect(true).toBeTruthy();
+  test('Create & Get', async () => {
+    const createdReservation = await createReservation();
+    const responseGet = await fetch(
+      `http://reservations:3000/reservations/${createdReservation._id}`,
+      {
+        headers: {
+          Authentication: jwt,
+        },
+      },
+    );
+    const reservation = await responseGet.json();
+    expect(createdReservation).toEqual(reservation);
   });
+
+  const createReservation = async () => {
+    const responseCreate = await fetch(
+      'http://reservations:3000/reservations',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authentication: jwt,
+        },
+        body: JSON.stringify({
+          startDate: '12/20/2020',
+          endDate: '12/30/2020',
+          charge: {
+            amount: 40,
+            card: {
+              cvc: '413',
+              exp_month: 12,
+              exp_year: 2027,
+              number: '4242 4242 4242 4242',
+            },
+          },
+        }),
+      },
+    );
+
+    expect(responseCreate.ok).toBeTruthy();
+    return responseCreate.json();
+  };
 });
